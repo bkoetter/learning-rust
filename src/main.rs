a5b035640ef3ed41c13aee54b5653b700dfcad35
Björn Kötter <git@bk.netburn.de>
2023-12-25 17:17:06 +0100

    working improved version

--- src/main.rs
+++ src/main.rs
@@ -8,43 +8,40 @@ struct Draw {
 
 impl Debug for Draw {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.blue > 0 {
-            write!(f, "{} blue, ", self.blue)?;
-        }
-        if self.green > 0 {
-            write!(f, "{} green, ", self.green)?;
-        }
-        if self.red > 0 {
-            write!(f, "{} red, ", self.red)?;
-        }
+        write!(f, "{} red, ", self.red)?;
+        write!(f, "{} blue, ", self.blue)?;
+        write!(f, "{} green, ", self.green)?;
         Ok(())
     }
 }
 
-fn main() {
-    let game = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+fn parse_game(game: &str) -> (u16, Vec<Draw>) {
     let (game_id, s) = game.split_once(':')
-        .map(|(id, s)| (
+        .map(|(id, draws)| (
             id.trim().split_ascii_whitespace().nth(1).unwrap().parse::<u16>().unwrap(),
-            s.split(';')
-                .map(|x| x.split(',')
-                    .map(|x| x.trim())
-                    .map(|x| x.split_once(' ').map(|(n, color)| (n.parse::<u8>().unwrap(), color)).unwrap())
-                    .map(|(n, color)|
-                        match color {
-                            "red" => Draw { blue: 0, green: 0, red: n },
-                            "blue" => Draw { blue: n, green: 0, red: 0 },
-                            "green" => Draw { blue: 0, green: n, red: 0 },
-                            _ => panic!("Unknown color: {color}"),
-                        })
+            draws.split(';')
+                .map(|draw| draw.split(',')
+                    .map(|s| s.trim().split_once(' ')
+                        .map(|(n, color)|
+                            match color {
+                                "red" => Draw { blue: 0, green: 0, red: n.parse::<u8>().unwrap() },
+                                "blue" => Draw { blue: n.parse::<u8>().unwrap(), green: 0, red: 0 },
+                                "green" => Draw { blue: 0, green: n.parse::<u8>().unwrap(), red: 0 },
+                                _ => panic!("Unknown color: {color}"),
+                            }).unwrap())
                     .fold(Draw { blue: 0, green: 0, red: 0 }, |acc, draw| Draw {
                         blue: acc.blue + draw.blue,
                         green: acc.green + draw.green,
                         red: acc.red + draw.red,
-                    })
-                )
+                    }))
                 .collect::<Vec<_>>()
         )).unwrap();
+    (game_id, s)
+}
+
+fn main() {
+    let game = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+    let (game_id, s) = parse_game(game);
 
     assert_eq!(game_id, 3);
     println!("{:?}", game_id, );
